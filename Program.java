0477a9861bfaef9fffdb87838b59f85d73b33e83
cemtaskin <cemtaskin82@gmail.com>
2023-02-23 20:35:05 +0300

    Print and PrintLn Methods

--- Program.java
+++ Program.java
@@ -1,7 +1,15 @@
 public class Program {
     
     public static void main (String[] args){
+
         System.out.println("Hello World");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+
+        System.out.print("Hello World");
+        System.out.print("I am learning Java.");
+        System.out.print("It is awesome!");
+        
     }
 
 }
