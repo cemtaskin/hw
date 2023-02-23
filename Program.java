8b320793ab1067be5c3b1ee25c4674571aa99bf0
cemtaskin <cemtaskin82@gmail.com>
2023-02-23 20:52:56 +0300

    Java Comments

--- Program.java
+++ Program.java
@@ -2,14 +2,38 @@ public class Program {
     
     public static void main (String[] args){
 
+        //This is a comment
+
         System.out.println("Hello World");
         System.out.println("I am learning Java.");
         System.out.println("It is awesome!");
-
+        /* 
         System.out.print("Hello World");
         System.out.print("I am learning Java.");
         System.out.print("It is awesome!");
         
+        
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        */
+
+        System.out.println(3+3);
+        System.out.println(2*5);
+        System.out.println(add(4,10));
     }
 
+    /**
+     * Add two numbers and returns the result
+     * @param a first number to add
+     * @param b second number to add
+     * @return addition result
+     */
+    public static int add (int a, int b){
+        return a+b;
+    }
+
+    
+
 }
+
